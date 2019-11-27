e7ef370413ad53b32143b60026d056999e5e629f
Gabriel Costa <dev@micromart.com.br>
2019-11-27 12:42:17 -0300

    cadastrando categoria no banco de dados

--- categories/CategoryController.js
+++ categories/CategoryController.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const slugify = require('slugify');
 const router = express();
+const Category = require('./Category');
 
 router.get('/admin/categories/create', (req, res) => {
     res.render('admin/categories/create');
+});
+
+router.post('/admin/categories', (req, res) => {
+    const title = req.body.title;
+
+    if(title != undefined) {
+        Category.create({
+            title,
+            slug: slugify(title).toLowerCase(),
+        })
+            .then(() => {
+                res.redirect('/');
+            });
+    } else {
+        res.redirect('/admin/categories/create');
+    }
 });
 
 module.exports = router;
