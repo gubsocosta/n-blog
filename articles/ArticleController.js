64746136939bd831f88a60f1afb3eaa06a00bcf6
Gabriel Costa <dev@micromart.com.br>
2019-12-16 12:00:38 -0300

    criando o formulario de cadastro de artigos

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express();
 const pathname = '/admin/articles'
+const Category = require('../categories/Category')
 
 // create
 router.get(pathname + '/create', (req, res) => {
-    res.render('admin/articles/create');
+    Category.findAll()
+        .then((categories) => {
+            res.render('admin/articles/create', { categories });
+        });
 });
 
 module.exports = router;
