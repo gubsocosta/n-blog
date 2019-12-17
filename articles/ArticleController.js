5537f44772560e5aff782466608ed528c1f6bab6
Gabriel Costa <dev@micromart.com.br>
2019-12-17 10:12:22 -0300

    salvando artigos nos banco de dados

--- articles/ArticleController.js
+++ articles/ArticleController.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express();
-const pathname = '/admin/articles'
-const Category = require('../categories/Category')
+const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
+const pathname = '/admin/articles';
+
+// index
+router.get(pathname, (req, res) => {
+    res.send('Index - Categoria');
+});
 
 // create
 router.get(pathname + '/create', (req, res) => {
     Category.findAll()
         .then((categories) => {
             res.render('admin/articles/create', { categories });
+        });
+});
+
+// store
+router.post(pathname, (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const categoryId = req.body.categoryId;
+
+    Article.create({
+        title,
+        slug: slugify(title).toLowerCase(),
+        body,
+        categoryId,
+    })
+        .then(() => {
+            res.redirect('admin/articles');
         });
 });
 
--- database/database.js
+++ database/database.js
@@ -4,11 +4,13 @@ const user = 'nblog_local';
 const password = 'secret';
 const host = 'localhost';
 const dialect = 'mysql';
+const timezone = '-03:00';
 const port = 3306;
 
 const connection = new Sequelize(database, user, password, {
     host,
     dialect,
+    timezone,
 });
 
 module.exports = connection;
